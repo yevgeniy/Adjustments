294ebd387a75d727fef0b1c797c1b7fd421b0fbb
Gene Andreyev <gene.a@digitalroominc.com>
2024-03-04 21:15:30 -0600

    asdf

--- Adjustments/SubjugateComp.cs
+++ Adjustments/SubjugateComp.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using Verse;
 
 namespace Adjustments
@@ -10,6 +12,20 @@ namespace Adjustments
     [StaticConstructorOnStartup]
     public class SubjugateComp : ThingComp
     {
+        private float CurrentRating;
+        private float ResistanceCap;
+        public static readonly TraitDef subjugatedTrait = DefDatabase<TraitDef>.GetNamed("Subjugated");
+        private Pawn Pawn
+        {
+            get
+            {
+                if (parent is Pawn pawn)
+                {
+                    return pawn;
+                }
+                return null;
+            }
+        }
         static SubjugateComp()
         {
             foreach (ThingDef thingDef in DefDatabase<ThingDef>.AllDefs.Where(thingDef =>
@@ -18,10 +34,37 @@ namespace Adjustments
                 thingDef.comps.Add(new CompProperties { compClass = typeof(SubjugateComp) });
             }
         }
+        public SubjugateComp()
+        {
+            CurrentRating = 0f;
+            ResistanceCap = 0f;
+        }
 
         public void ActivateSubjugation()
         {
+            CurrentRating = 0f;
+            if (Pawn==null)
+            {
+                Log.Error("Pawn not found on SubjugateComp.");
+                return;
+            }
+            ResistanceCap = Pawn.guest.resistance * 10f;
             Log.Message("SUBJUCATION ACTIVATE");
+        }
+
+        public void RegisterSeverity(float suffering)
+        {
+            CurrentRating = Mathf.Min(ResistanceCap, CurrentRating + suffering);
+
+            if (CurrentRating>=ResistanceCap)
+            {
+                ResistanceCapBreached();
+            }
+        }
+
+        private void ResistanceCapBreached()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Adjustments/SubjugatePatch.cs
+++ Adjustments/SubjugatePatch.cs
@@ -10,9 +10,45 @@ using Verse;
 
 namespace Adjustments
 {
+    //public void AddDirect(Hediff hediff, DamageInfo? dinfo = null, DamageWorker.DamageResult damageResult = null)
+    [HarmonyPatch(typeof(HediffSet), "AddDirect")]
+    public class register_severity_for_beating
+    {
+        [HarmonyPrefix]
+        public static bool Patch(Hediff hediff, DamageInfo dinfo, DamageWorker.DamageResult damageResult, HediffSet __instance)
+        {
+            if (hediff!=null)
+            {
+                var pawn = __instance.pawn;
+                if (pawn.gender == Gender.Female)
+                {
+                    var comp = pawn.GetComp<SubjugateComp>();
+                    if (comp != null)
+                    {
+                        comp.RegisterSeverity(hediff.Severity);
+                    }
+                }
+
+            }
+
+            return true;
+        }
+
+        private static Pawn GetPawn(Pawn_GuestTracker instance)
+        {
+            Type type = typeof(Pawn_GuestTracker);
+
+            // Get the private field info
+            FieldInfo fieldInfo = type.GetField("pawn", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            return (Pawn)fieldInfo.GetValue(instance);
+
+        }
+    }
+
     //public void SetGuestStatus(Faction newHost, GuestStatus guestStatus = 0)
     [HarmonyPatch(typeof(Pawn_GuestTracker), "SetGuestStatus")]
-    public class PawnGuestTracker_SetGuestStatus
+    public class register_prisoner_start
     {
         [HarmonyPrefix]
         public static bool Patch(Faction newHost, GuestStatus guestStatus, Pawn_GuestTracker __instance)
