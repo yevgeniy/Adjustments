3ebfae7595cdcd63ecfe964e425728486c2385cd
Gene Andreyev <gene.a@digitalroominc.com>
2024-04-11 23:04:58 -0500

    asdf

---
+++ Adjustments/Haul_Adjustments.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Adjustments
+{
+    [StaticConstructorOnStartup]
+    public class Haul_Adjustments
+    {
+        static Haul_Adjustments()
+        {
+            var delivertoframe = DefDatabase<WorkGiverDef>.AllDefs.First(v => v.defName == "DeliverResourcesToFrames");
+            delivertoframe.priorityInType = 17;
+
+            var delivertoprints = DefDatabase<WorkGiverDef>.AllDefs.First(v => v.defName == "DeliverResourcesToBlueprints");
+            delivertoprints.priorityInType = 16;
+
+            var haulwork = DefDatabase<WorkTypeDef>.AllDefs.First(v => v.defName == "Hauling");
+            haulwork.workGiversByPriority.Clear();
+            haulwork.ResolveReferences();
+
+
+        }
+    }
+}
